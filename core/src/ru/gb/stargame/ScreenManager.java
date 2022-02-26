a3171750b12cc4f198405dc1fb78bfb0fa8b57c7
nekrut <nekrut12@gmail.com>
2022-02-26 18:55:42 +0300

    Initial commit

---
+++ core/src/ru/gb/stargame/ScreenManager.java
@@ -0,0 +1,8 @@
+package ru.gb.stargame;
+
+public class ScreenManager {
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+    public static final int STARS_COUNT = 500;
+
+}
